a0c194624fc842e98ca2ff28ab180f552acb98d2
Maciej Józefczyk <mjozefcz@redhat.com>
2020-03-27 10:41:00 +0000

    Respect SSL devstack configuration
    If devstack is spawned with SSL, that means we need
    to configure OVN databases connectors to use TLS.
    
    Change-Id: I4f95a6ea0f299700281e3dde26ebcaba77a1b1fc

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,11 +3,24 @@
 # devstack plugin for octavia
 GET_PIP_CACHE_LOCATION=/opt/stack/cache/files/get-pip.py
 
-# [api_settings]
-#enabled_provider_drivers = amphora:'The Octavia Amphora driver.',ovn:'Octavia OVN driver.'
+# How to connect to ovsdb-server hosting the OVN NB database
+if is_service_enabled tls-proxy; then
+   OVN_PROTO=ssl
+else
+   OVN_PROTO=tcp
+fi
+OVN_NB_REMOTE=${OVN_NB_REMOTE:-$OVN_PROTO:$SERVICE_HOST:6641}
 
 function _configure_provider_driver {
     iniset ${OCTAVIA_CONF} api_settings enabled_provider_drivers ${OCTAVIA_PROVIDER_DRIVERS}
+    iniset ${OCTAVIA_CONF} ovn ovn_nb_connection "$OVN_NB_REMOTE"
+
+    if is_service_enabled tls-proxy; then
+        iniset ${OCTAVIA_CONF} ovn ovn_nb_connection "$OVN_NB_REMOTE"
+        iniset ${OCTAVIA_CONF} ovn ovn_nb_ca_cert "$INT_CA_DIR/ca-chain.pem"
+        iniset ${OCTAVIA_CONF} ovn ovn_nb_certificate "$INT_CA_DIR/$DEVSTACK_CERT_NAME.crt"
+        iniset ${OCTAVIA_CONF} ovn ovn_nb_private_key "$INT_CA_DIR/private/$DEVSTACK_CERT_NAME.key"
+    fi
 }
 
 function is_ovn_enabled {
