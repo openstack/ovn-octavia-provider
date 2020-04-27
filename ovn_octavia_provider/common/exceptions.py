aca9eab6f77ed65e1ca91301d32acf2bc264baf3
Brian Haley <bhaley@redhat.com>
2020-04-27 15:05:00 -0400

    Re-home exceptions to common/exceptions.py
    Change-Id: I2db28dc025f256b5b818ddbfd88a818388d09af0

--- ovn_octavia_provider/common/exceptions.py
+++ ovn_octavia_provider/common/exceptions.py
@@ -14,6 +14,7 @@
 #    under the License.
 
 from neutron_lib import exceptions as n_exc
+from octavia_lib.api.drivers import exceptions as driver_exceptions
 
 from ovn_octavia_provider.i18n import _
 
@@ -22,3 +23,10 @@ class RevisionConflict(n_exc.NeutronException):
     message = _('OVN revision number for %(resource_id)s (type: '
                 '%(resource_type)s) is equal or higher than the given '
                 'resource. Skipping update')
+
+
+class IPVersionsMixingNotSupportedError(
+        driver_exceptions.UnsupportedOptionError):
+    user_fault_string = _('OVN provider does not support mixing IPv4/IPv6 '
+                          'configuration within the same Load Balancer.')
+    operator_fault_string = user_fault_string
