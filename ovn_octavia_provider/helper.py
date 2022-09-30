4f4c80c98e160a415d5b5129e40f0018811a68d2
Luis Tomas Bolivar <ltomasbo@redhat.com>
2022-09-30 20:13:49 +0200

    Ensure lbs are properly configured for router gateway set/unset
    Before adding support for lbs with VIPs on the provider networks,
    there was no need to react to gateway chassis creation events, and
    nothing was done for its deletion. However, after adding the support
    for that, there is a need of properly handling the creation event
    for that type of port.
    
    For the loadbalancer VIPs on the provider network, processing the
    event and triggering the lb_creat_lrp_assoc_handler means that the
    information about the logical router will be added, i.e., the router
    is added to the loadbalancer external_ids as a lr_ref, while the
    loadbalancer is also added into the logical_router object
    (loadbalancer entry). In addition, the lb is also added to the logical
    switches connected to the router.
    
    For the loadbalancer VIPs on tenant networks (which should not be
    affected by the gateway port creation/deletion), this patch ensures
    the lb is not added to the logical switch representing the provider
    network that is connected to the router. Therefore differentiating
    between lrp ports which has gateway_chassis and the ones that don't,
    i.e., adding the lb to the switch when the lrp port is the one
    connecting a subnet with the router, and not doing so when it is the
    gateway port for the router to the provider network.
    
    Closes-Bug: #1991509
    
    Change-Id: Iddd96fd9015230b3dd75aa2182055cf43eb608c1
    (cherry picked from commit d889812f3c2deaf630892d65c90cdbafd4435d21)

--- ovn_octavia_provider/helper.py
+++ ovn_octavia_provider/helper.py
@@ -241,7 +241,8 @@ class OvnProviderHelper():
             LOG.debug("Router or network information not found")
             return
         request_info = {'network': network,
-                        'router': router}
+                        'router': router,
+                        'gateway_chassis': row.gateway_chassis}
         self.add_request({'type': ovn_const.REQ_TYPE_LB_CREATE_LRP_ASSOC,
                           'info': request_info})
 
@@ -257,6 +258,15 @@ class OvnProviderHelper():
                             "logical router %s failed, trying step by step",
                             lb.uuid, info['router'].uuid)
                 self._update_lb_to_lr_association_by_step(lb, info['router'])
+
+        # if gateway_chassis ports, there is no need to re-add the
+        # loadbalancers from the router into the provider network.
+        # This will be already done for loadbalancer created with VIPs on
+        # provider networks, regardless of the gateway_chassis lrp port
+        # existence. And it should never be there when the VIPs are on
+        # tenant networks.
+        if info['gateway_chassis']:
+            return
 
         # Add those lb to the network which are unique to the router
         for lb in (router_lb - network_lb):
